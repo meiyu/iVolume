fd40d5f5157fbfb08f219658d2fc65886adccdcc
Ren Zihou <renzihou.bj@gmail.com>
2022-11-19 17:01:04 +0800

    noise adjuster (feedback undone)

--- app/src/main/java/com/ivolume/ivolume/VolumeUpdater.java
+++ app/src/main/java/com/ivolume/ivolume/VolumeUpdater.java
@@ -17,32 +17,40 @@ import java.lang.Math;
 
 
 class ContextInfo {
-    public final int gps;
-    public final int app;
-    public final boolean plugged;
-    public final float noise;
-    private final int hash;
+    public int gps;
+    public int app;
+    public boolean plugged;
 
-    ContextInfo(int gps, int app, boolean plugged, float noise) {
+    ContextInfo(int gps, int app, boolean plugged) {
         this.gps = gps;
         this.app = app;
         this.plugged = plugged;
-        this.noise = noise;
-        this.hash = 193 * gps + 24593 * app + (plugged ? 53 : 0);  // does not consider noise
     }
 
     @Override
     public boolean equals(Object other) {
         if (other == null || getClass() != other.getClass()) return false;
         ContextInfo o = (ContextInfo) other;
-        int noiseErr = 1;  // TODO change noise error tolerance
-        return this.gps == o.gps && this.app == o.app && this.plugged == o.plugged
-                && Math.abs(this.noise - o.noise) < noiseErr;
+        return this.gps == o.gps && this.app == o.app && this.plugged == o.plugged;
     }
 
     @Override
     public int hashCode() {
-        return this.hash;
+        return 193 * gps + 24593 * app + (plugged ? 53 : 0);
+    }
+}
+
+
+class NoiseAdjuster {
+    private double noise_a_plugged = 0;
+    private double noise_b_plugged = 0;
+    private double noise_a_unplugged = 0;
+    private double noise_b_unplugged = 0;
+
+    // f(noise) = round(a * noise + b)
+    public int adjust(double noise, boolean plugged) {
+        return (int) Math.round((plugged ? noise_a_plugged : noise_a_unplugged) * noise
+                + (plugged ? noise_b_plugged : noise_b_unplugged));
     }
 }
 
@@ -51,6 +59,7 @@ public class VolumeUpdater extends Service {
     public static VolumeUpdater mVolumeUpdater;
     private AudioManager mAudioManager;
     private final HashMap<ContextInfo, Integer> mMap;
+    private final NoiseAdjuster mNoiseAdjuster;
     private final static double lambda = 0.5;
     private final static double mu = 0.2;
 
@@ -63,6 +72,7 @@ public class VolumeUpdater extends Service {
 
     public VolumeUpdater() {
         this.mMap = new HashMap<>();
+        this.mNoiseAdjuster = new NoiseAdjuster();
     }
 
     @Override
@@ -73,19 +83,19 @@ public class VolumeUpdater extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-//        this.mAudioManager = (AudioManager) this.mContext.getSystemService(Context.AUDIO_SERVICE);
     }
 
-    public void updateTable(Context context, int gps, int app, boolean plugged, float noise, int qa_result) {
+    public void updateTable(Context context, int gps, int app, boolean plugged, double noise, int qa_result) {
         if (mAudioManager == null) {
-            mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+            mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         }
         // single entry
-        Integer old_target = this.mMap.get(new ContextInfo(gps, app, plugged, noise));
+        Integer old_target = this.mMap.get(new ContextInfo(gps, app, plugged));
         int current_volume = this.mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int current_target = current_volume - this.mNoiseAdjuster.adjust(noise, plugged);
         if (old_target != null) {
-            int new_target = (int) Math.round(current_volume * lambda + old_target * (1 - lambda));
-            this.mMap.replace(new ContextInfo(gps, app, plugged, noise), new_target);
+            int new_target = (int) Math.round(current_target * lambda + old_target * (1 - lambda));
+            this.mMap.replace(new ContextInfo(gps, app, plugged), new_target);
             Log.d("VUT", String.format("change context <gps=%d, app=%d, plugged=%b, noise=%.2f> from <%d> to <%d>",
                     gps, app, plugged, noise, old_target, new_target));
         } else {  // regard as not-hit context and we don't want to insert it
@@ -95,24 +105,39 @@ public class VolumeUpdater extends Service {
         // volume *= (1 + alpha * mu)
         // alpha = delta / old_target
         switch (qa_result) {
-            case -1:
-                return;
             case 0:  // gps
+                for (HashMap.Entry<ContextInfo, Integer> entry : this.mMap.entrySet()) {
+                    if (entry.getKey().gps == gps) {
+                        entry.setValue((int) Math.round(entry.getValue() * (1 + mu * (current_target - old_target) / old_target)));
+                        Log.d("VUT", String.format("change context <gps=%d, app=%d, plugged=%b, noise=%.2f> to <%d>",
+                                entry.getKey().gps, entry.getKey().app, entry.getKey().plugged, 0.0, entry.getValue()));
+                    }
+                }
+                break;
             case 1:  // app
+                for (HashMap.Entry<ContextInfo, Integer> entry : this.mMap.entrySet()) {
+                    if (entry.getKey().app == app) {
+                        entry.setValue((int) Math.round(entry.getValue() * (1 + mu * (current_target - old_target) / old_target)));
+                        Log.d("VUT", String.format("change context <gps=%d, app=%d, plugged=%b, noise=%.2f> to <%d>",
+                                entry.getKey().gps, entry.getKey().app, entry.getKey().plugged, 0.0, entry.getValue()));
+                    }
+                }
+                break;
             case 2:  // plugged
                 for (HashMap.Entry<ContextInfo, Integer> entry : this.mMap.entrySet()) {
                     if (entry.getKey().plugged == plugged) {
-                        entry.setValue((int) Math.round(entry.getValue() * (1 + mu * (current_volume - old_target) / old_target)));
+                        entry.setValue((int) Math.round(entry.getValue() * (1 + mu * (current_target - old_target) / old_target)));
                         Log.d("VUT", String.format("change context <gps=%d, app=%d, plugged=%b, noise=%.2f> to <%d>",
                                 entry.getKey().gps, entry.getKey().app, entry.getKey().plugged, 0.0, entry.getValue()));
                     }
                 }
                 break;
             case 3:  // noise
-        }
 
-        // if q != -1
-        // map context
+                break;
+            default:
+                break;
+        }
     }
 
     // new_volume = target + f(noise, plugged)
@@ -120,14 +145,15 @@ public class VolumeUpdater extends Service {
         if (mAudioManager == null) {
             mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         }
-        Integer target = this.mMap.get(new ContextInfo(gps, app, plugged, noise));
+        Integer target = this.mMap.get(new ContextInfo(gps, app, plugged));
         int current_volume = this.mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
         if (target == null) {  // does not contain current context
-            this.mMap.put(new ContextInfo(gps, app, plugged, noise), current_volume);
+            this.mMap.put(new ContextInfo(gps, app, plugged), current_volume - this.mNoiseAdjuster.adjust(noise, plugged));
             Log.d("VU", String.format("add new context <gps=%d, app=%d, plugged=%b, noise=%.2f>, now contains %d entries",
                     gps, app, plugged, noise, this.mMap.size()));
 
         } else {
+            target = target + this.mNoiseAdjuster.adjust(noise, plugged);
             int steps = Math.abs(target - current_volume);
             int direction = target > current_volume ? AudioManager.ADJUST_RAISE
                     : AudioManager.ADJUST_LOWER;
