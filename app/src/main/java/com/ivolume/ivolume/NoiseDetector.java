343c70acb634386688bb587c24bcfe07f6807b17
lwx <liuwx20@mails.tsinghua.edu.cn>
2022-11-19 14:45:11 +0800

    noise_detector fixed

--- app/src/main/java/com/ivolume/ivolume/NoiseDetector.java
+++ app/src/main/java/com/ivolume/ivolume/NoiseDetector.java
@@ -1,108 +1,89 @@
 package com.ivolume.ivolume;
 
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.util.Log;
-import android.app.Service;
-import android.content.Intent;
-import android.media.AudioManager;
-import android.os.IBinder;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
+ public class NoiseDetector {
 
-public class NoiseDetector {
+    private static final String TAG = "AudioRecord";
+    static final int SAMPLE_RATE_IN_HZ = 8000;
+    static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ,
+    AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
+    AudioRecord mAudioRecord;
+    boolean isGetVoiceRun;
+    Object mLock;
+    public Integer volume_level;
+    public double volume;
+    public NoiseDetector() {
+         mLock = new Object();
+     }
 
-    private final long initialDelay = 5000;
-    private final long samplePeriod = 10;  // sample max amplitude every 10ms
-    private final int intervalDetection = 2000; // detect db every 2s
-    private static final String TAG = "NoiseDetector";
-    public double lastNoise = 0;
-    private double lastTriggerNoise = 0;
-    private boolean hasFirstDetection = false;
-    private long lastTimestamp = 0;
-    private final double threshold = 20;
-    public static Integer latest_noiseLevel;
+     //default : 0; low : 1; mid : 2; high : 3
+     public Integer getNoiseLevel(double db) {
+         if (db <= 0)
+             return 0;
+         else if (db <= 35)
+             return 1;
+         else if (db <= 60)
+             return 2;
+         else
+             return 3;
+     }
 
-    //default : 0; low : 1; mid : 2; high : 3
-    public Integer getNoiseLevel(double db) {
-        if (db <= 0)
-            return 0;
-        else if (db <= 35)
-            return 1;
-        else if (db <= 60)
-            return 2;
-        else
-            return 3;
-    }
-    /* set noise */
-    private void setPresentNoise(double noise) {
-        lastNoise = noise;
-        latest_noiseLevel = getNoiseLevel(noise);
-    }
-
-    private double getAvgNoiseFromSeq(List<Integer> seq) {
-        double BASE = 1.0; // 32768
-        double sum = 0.0;
-        double sum_squared = 0.0;
-        int count = 0;
-
-        int idx = 0;
-        double db;
-        int next_idx;
-        double next_db;
-        int maxAmplitude = 0;
-        int current_maxAmplitude;
-
-        // 找到第一个非零值
-        while (idx < seq.size() && (maxAmplitude = seq.get(idx)) == 0) {
-            idx++;
+     public void get_volume() {
+        if (isGetVoiceRun) {
+            Log.e(TAG, "还在录着呢");
         }
-        db = 20 * Math.log10(maxAmplitude / BASE);
-        current_maxAmplitude = maxAmplitude;
-        next_idx = idx + 1;
-//            Log.e(TAG, "getNoiseLevel: " + String.format("idx: %d maxAmplitude: %d db: %f", idx, maxAmplitude, db));
-        // 采样为0时使用两边非零值线性插值
-        while (true) {
-            while (next_idx < seq.size() && (maxAmplitude = seq.get(next_idx)) == 0) {
-                next_idx++;
-            }
-            if (next_idx >= seq.size()) {
-                sum += db;
-                sum_squared += current_maxAmplitude * current_maxAmplitude;
-                count += 1;
-                break;
-            }
-            next_db = 20 * Math.log10(maxAmplitude / BASE);
-            sum += db + (db + next_db) * 0.5 * (next_idx - idx - 1);
-            double interp = (current_maxAmplitude + maxAmplitude) * 0.5;
-            sum_squared += current_maxAmplitude * current_maxAmplitude + interp * interp * (next_idx - idx - 1);
-            count += next_idx - idx;
-
-            idx = next_idx++;
-            db = next_db;
-            current_maxAmplitude = maxAmplitude;
+        try{
+            mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    SAMPLE_RATE_IN_HZ, AudioFormat.CHANNEL_IN_DEFAULT,
+                    AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE);
+        }catch (SecurityException e){
+            e.printStackTrace();
         }
+        if (mAudioRecord == null) {
+            Log.e("sound", "mAudioRecord初始化失败");
+        }
+        isGetVoiceRun = true;
 
-        double rms = Math.sqrt(sum_squared / count);
-        double average_noise = (count > 0)? (sum / count) : 0.0;
-
-        Log.e(TAG, String.format("getAvgNoiseFromSeq: %d sampled, average %fdb", count, average_noise));
-        return average_noise;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mAudioRecord.startRecording();
+                short[] buffer = new short[BUFFER_SIZE];
+                int noise_count = 1;
+                volume = 0;
+                while (isGetVoiceRun && noise_count <= 10) {
+                    int r = mAudioRecord.read(buffer, 0, BUFFER_SIZE);
+                    long v = 0;
+                    for (int i = 0; i < buffer.length; i++) {
+                        v += buffer[i] * buffer[i];
+                    }
+                    double mean = v / (double) r;
+                    volume = (10 * Math.log10(mean) + volume * noise_count) / (noise_count + 1);
+                    volume_level = getNoiseLevel(volume);
+                    Log.d(TAG, "db:" + volume);
+                    Log.d(TAG, "db level:" + volume_level);
+                    // 大概一秒十次
+                    synchronized (mLock) {
+                        try {
+                            mLock.wait(100);
+                            noise_count ++;
+                        } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        }
+                    }
+                }
+                mAudioRecord.stop();
+                mAudioRecord.release();
+                mAudioRecord = null;
+            }
+        }).start();
     }
-    
-    float getNoise() {  // main entrance, call this on
-        return latest_noiseLevel;
-    }
-}
+     public  double getNoise() {  // main entrance, call this on
+        get_volume();
+        return volume;
+     }
+ }
